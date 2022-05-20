c3ef5bcacc67add90ae43281ad2c33d7d6f4590c
megahigh5 <102623149+megahigh5@users.noreply.github.com>
2022-05-20 16:08:56 -0500

    Add files via upload

---
+++ folder/Day4Exceptions/AgeException.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day4Exceptions
+{
+    public class AgeException : ApplicationException
+    {
+        public override string Message => "Age must be between 18-65";
+    }
+
+    public class Employee
+    {
+
+        public int Id { get; set; }
+        public int Age
+        {
+            get { return Age; }
+
+            set
+            {
+                if (value < 18 || value > 65) { throw new AgeException(); }
+                Age = value;
+            }
+        }
+    }
+}
---
+++ folder/Day4Exceptions/Program.cs
@@ -0,0 +1,52 @@
+﻿using Day4Exceptions;
+
+namespace Day4Expections
+{
+    public class Program
+    {
+        //Exceptions are errors done in runtime
+        public static void Main(string[] args)
+        {
+            try
+            {
+                //var i = Console.ReadLine();
+                //int detect = Convert.ToInt32(i);
+                //int letOver = checked(Int32.MaxValue + detect);
+                Employee emp = new Employee();
+                //emp.Age = 75;
+
+
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (OverflowException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (AgeException ex)
+            {
+                Console.WriteLine("This was our custom Excpetion");
+                Console.WriteLine(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("No matter what, i will run");
+            }
+            Console.WriteLine("Still running");
+
+
+            //using (Connection)
+            //{
+            //    int o = 0;
+            //}
+
+            
+        }
+    }
+}
