78fb2aac974a4822241a4b3150fd2dd592f07623
megahigh5 <102623149+megahigh5@users.noreply.github.com>
2022-05-20 16:07:22 -0500

    Add files via upload

---
+++ folder/Day3Collections/ManageEmployee.cs
@@ -0,0 +1,48 @@
+﻿using Day3Collections.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day3Collections
+{
+    public class ManageEmployee
+    {
+        List<Employee> empCollection;
+        public ManageEmployee()
+        {
+            empCollection = new List<Employee>();
+        }
+
+        public void AddEmployee()
+        {
+            Employee e = new Employee();
+            e.Id = 1;
+            e.Name = "smith";
+            e.Salary = 1234;
+            e.DeptId = 1;
+            e.Age = 40;
+            empCollection.Add(e);
+
+            Employee e1 = new Employee()
+            {
+                Id = 2,
+                Name = "Will",
+                Salary = 4567,
+                DeptId = 1,
+                Age = 18
+            };
+            empCollection.Add(e1);
+
+            empCollection.Add(new Employee() { Id = 3, Name = "Steven", Salary = 9876, DeptId =2, Age = 43});
+        }
+        public void PrintEmployees()
+        {
+            foreach (Employee e in empCollection)
+            {
+                Console.WriteLine($"{e.Id} \t {e.Name} \t {e.Age} \t {e.Salary} \t {e.DeptId} ");
+            }
+        }
+    }
+}
---
+++ folder/Day3Collections/Program.cs
@@ -0,0 +1,48 @@
+﻿using Day3Collections.Entities;
+
+namespace Day3Collections
+{
+    public class Program
+    {
+
+        public static void Main(string[] args)
+        {
+            //int[] arrInt = new int[]{ 4, 3, 2, 1 };
+
+            //for (int i = 0; i < arrInt.Length; i++)
+            //{
+            //    Console.WriteLine( arrInt[i]);
+            //}
+
+            //foreach(int i in arrInt)
+            //{
+            //    Console.WriteLine( i);
+            //}
+
+            //var enumerator = arrInt.GetEnumerator();
+
+            //var enumerator2 = arrInt.GetEnumerator(); //This is considered a different enumerator for the same array
+            //enumerator2.MoveNext();
+            //while (enumerator.MoveNext())
+            //{
+            //    Console.WriteLine(enumerator.Current);
+            //    Console.WriteLine(enumerator2.Current); 
+            //}
+
+            ManageEmployee manageEmployee = new ManageEmployee();
+            manageEmployee.AddEmployee();
+            manageEmployee.PrintEmployees();
+
+
+            Dictionary<string, Employee> empCollection = new Dictionary<string, Employee>();
+            empCollection.Add("William", new Employee() { Id = 1, Name = "William", Salary = 9876, DeptId = 2, Age = 43 });
+            foreach(var emp in empCollection)
+            {
+                Console.WriteLine(emp.Key + " " + emp.Value.Salary);
+            }
+            if (empCollection.ContainsKey("William")){
+                empCollection.Remove("William");
+            }
+        }
+    }
+}
