8e6d36505521ff8c8ba3309d7217d6f0d9569828
megahigh5 <102623149+megahigh5@users.noreply.github.com>
2022-06-01 13:42:57 -0500

    Add files via upload

---
+++ HTML_CSS FOLDER/Javascript/scripts/basics.js
@@ -0,0 +1,12 @@
+//alert("Hello World");
+
+// javascript is a weakly typed language
+
+let variable = 0;
+console.log(variable);
+variable = true;
+console.log(variable);
+variable = "I am a string now";
+console.log(variable);
+
+//Typescript: a (more) strongly typed languague as a super set of javascript
